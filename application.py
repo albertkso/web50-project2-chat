eec01d26e6fa7802fbf55a5ce1c64af2f7354956
Albert So <ak_so@hotmail.com>
2018-03-10 09:20:30 +0800

    Added basic signin and signout

--- application.py
+++ application.py
@@ -1,13 +1,42 @@
 import os
 
-from flask import Flask
+from flask import Flask, redirect, render_template, request, session, url_for
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+# Configure a secret key for sessions
+app.secret_key = os.urandom(24)
+
 socketio = SocketIO(app)
 
+USER_SIGNED_IN = 'user_signed_in'
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+
+    return render_template('_home.html')
+
+
+@app.route("/signin", methods=['POST'])
+def signin():
+
+    username = request.form.get('userid')
+    session[USER_SIGNED_IN] = username
+
+    return render_template('_home.html')
+
+@app.route("/signout", methods=['POST'])
+def signout():
+
+    session.clear()
+
+    return redirect(url_for('index'))
+
+
+
+def _is_signed_in():
+
+    return session[USER_SIGNED_IN]
+
