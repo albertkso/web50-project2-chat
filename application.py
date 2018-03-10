de59bd36916968bb9cdd526b69cfa5c6e3bda76f
Albert So <ak_so@hotmail.com>
2018-03-10 23:31:26 +0800

    Added success status to channel function

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, redirect, render_template, request, session, url_for
+from flask import Flask, jsonify, redirect, render_template, request, session, url_for
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -11,12 +11,12 @@ app.secret_key = os.urandom(24) # Configure a secret key for sessions
 socketio = SocketIO(app)
 
 USER_SIGNED_IN = 'user_signed_in'
-DEFAULT_CHATS  = [ 'spam1', 'spam2' ]
+all_channels  = [ 'spam1', 'spam2', 'spam3' ]
 
 @app.route("/")
 def index():
 
-    return render_template('_home.html', chats=DEFAULT_CHATS)
+    return render_template('_home.html', chats=all_channels)
 
 
 @app.route("/signin", methods=['POST'])
@@ -25,7 +25,7 @@ def signin():
     username = request.form.get('userid')
     session[USER_SIGNED_IN] = username
 
-    return render_template('_home.html', chats=DEFAULT_CHATS)
+    return redirect(url_for('index'))
 
 
 @app.route("/signout", methods=['POST'])
@@ -33,6 +33,13 @@ def signout():
 
     session.clear()
     return redirect(url_for('index'))
+
+
+@app.route("/manage_channels", methods=['POST'])
+def manage_channels():
+
+    channel = request.form.get('channel')
+    return jsonify(success=True, channel=channel)
 
 
 def _is_signed_in():
