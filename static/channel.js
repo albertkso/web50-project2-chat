ceda1912667b91bca8ad44bf42200b71a7bc47c9
Albert So <ak_so@hotmail.com>
2018-03-12 17:28:33 +0800

    Added code to select channel and receive and send messages

--- static/channel.js
+++ static/channel.js
@@ -2,15 +2,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
+ /*
+  * Receive any messages / updates from other chat clients and update
+  * local client
+  */
+
     socket.on('server broadcast', data => {
-        const messageDiv = document.createElement('div');
-        messageDiv.innerHTML = 
+        const messagediv = document.createElement('div');
+        messagediv.innerHTML = 
             `<div style='margin: 0.5em 0.5em;'>
                 <div style='font-weight: bold'> ${data.sender} </div>
                 <div style='font-size: 95%'> ${data.message} </div>
              </div>`;
-        document.querySelector('.content').append(messageDiv);
+        document.querySelector('.content').append(messagediv);
     });
+
+    socket.on('server send history', data => {
+        for (i = 0; i <= data.length; i++) {
+            console.log(data[i]);
+        }
+    });
+    
+ /*
+  * Create a new chat channel
+  */
 
     document.querySelector('#add_channel').onclick = () => {
         const data = new FormData();
@@ -35,17 +50,26 @@ document.addEventListener('DOMContentLoaded', () => {
         return false;
     };
 
+ /*
+  * Send message to chat server for broadcast on the active channel
+  */
+
     document.querySelector('#send_message').onclick = () => {
         const channel = 'general';
         const sender = document.getElementById('current_user').innerText;
         const message = document.querySelector('#message').value;
 
-        message_parameters = { channel : channel, sender: sender, message: message }
-        socket.emit('client send message', message_parameters)
         document.querySelector('#message').value = "";
+
+        message_parameters = { channel: channel, sender: sender, message: message };
+        socket.emit('client send message', message_parameters);
 
         return false;
     }
+
+ /*
+  * Configure the active chat channel
+  */
 
     document.querySelector('#channels').onclick = (evt) => {
         if (evt.target.tagName != 'LI')
@@ -53,17 +77,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const activeChannel = evt.target;
         const channelName = activeChannel.innerText;
-        const allChannels = document.querySelectorAll('.channel_item');
+        const allChannels = document.querySelectorAll('#channels li');
         const sender = document.getElementById('current_user').innerText;
 
         for (i = 0; i < allChannels.length; i++) {
-            allChannels[i].style.fontWeight = 'normal';
+            allChannels[i].classList.remove('active_channel');
         }
+        activeChannel.classList.add('active_channel');
 
-        activeChannel.style.fontWeight = 'bold';
-
-        message_parameters = { channel : channelName, sender: sender }
-        socket.emit('client select channel', message_parameters)
+        message_parameters = { channel: channelName, sender: sender };
+        socket.emit('client select channel', message_parameters);
     }
 
 })
