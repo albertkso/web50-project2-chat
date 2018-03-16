73c6e93abae4ca8fd6a66daa3107474997cf1a7d
Albert So <ak_so@hotmail.com>
2018-03-16 11:46:11 +0800

    Make event handler configuration additive

--- static/channel.js
+++ static/channel.js
@@ -2,34 +2,28 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const selectMenu = document.querySelector('select');
-
+    
  /*
   * Write the current chat channel to storage
   */
 
-    document.querySelector('select').onchange = () => {
-    
+    selectMenu.addEventListener('change', () => {
         let channelName = selectMenu[selectMenu.selectedIndex].value;
         localStorage.setItem('currentChannel', channelName);
-        
-    }
+    });
 
  /*
   * Retrieve the current chat channel from storage
   */
 
-    window.onload = () => {
-
+    window.addEventListener('load', () => {
         let currentChannel = localStorage.getItem('currentChannel');
-
         for (i = 0; i < selectMenu.options.length; i++) {
             if (selectMenu.options[i].value === currentChannel) {
                 selectMenu.selectedIndex = i;
                 break;
             }
         }
-
-    }
+    });
 
 });
-
