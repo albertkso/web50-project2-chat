3bd133e73daa8270c3ca74b5167018e388129245
Albert So <ak_so@hotmail.com>
2018-03-26 08:51:07 +0800

    File cleanup

--- static/login.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-    document.querySelector('#signin_form button').onclick = () => {
-        console.log('login button clicked');
-        username = document.querySelector('#userid').value;
-        localStorage.getItem('activeuser');
-    };
-
-});
--- static/logout.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-    document.querySelector('#signout_form button').onclick = () => {
-        console.log('logout button clicked');
-        username = document.querySelector('#current_user').value;
-        localStorage.setItem('activeuser', username);
-    };
-
-});
